8573866ad03efdaa3abad472a79c2997911c2843
cheropnai <111832468+cheropnai@users.noreply.github.com>
2023-07-10 22:40:03 +0300

    chat functions

---
+++ lib/views/chat_list_view.dart
@@ -0,0 +1,97 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:rescuereach/api_calls/dummy_server.dart';
+import 'package:rescuereach/components/my_button.dart';
+import 'package:rescuereach/services/auth/bloc/auth_bloc.dart';
+import 'package:rescuereach/services/auth/bloc/auth_event.dart';
+import 'package:rescuereach/utilities/dialogs/logout_dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../utils/config.dart';
+import 'chat_page_view.dart';
+
+class ChatListView extends StatefulWidget {
+  const ChatListView({super.key});
+
+  @override
+  State<ChatListView> createState() => _ChatListViewState();
+}
+
+enum MenuAction { logout }
+
+class _ChatListViewState extends State<ChatListView> {
+  //instance of Auth
+   final currentUser = FirebaseAuth.instance.currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 191, 166, 233),
+        actions: [
+          PopupMenuButton<MenuAction>(onSelected: (value) async {
+            switch (value) {
+              case MenuAction.logout:
+                final shouldLogout = await showLogoutDialog(context);
+                if (shouldLogout) {
+                  // ignore: use_build_context_synchronously
+                  context.read<AuthBloc>().add(const AuthEventLogOut());
+                }
+                break;
+            }
+          }, itemBuilder: (context) {
+            return const [
+              PopupMenuItem<MenuAction>(
+                value: MenuAction.logout,
+                child: Text('Log out'),
+              ),
+            ];
+          })
+        ],
+      ),
+      body: _buildUserList(),
+    );
+  }
+
+  Widget _buildUserList() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return const Text('error');
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Text('waiting');
+        }
+        return ListView(
+          children: snapshot.data!.docs
+              .map<Widget>((doc) => _buildUserListItem(doc))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  Widget _buildUserListItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+    if (currentUser!.email != data['email']) {
+      return ListTile(
+        title: data['email'],
+        onTap: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => ChatPage(
+                  receiverUserEmail: data['email'],
+                  receiverUserID: data['uid'],
+                ),
+              ));
+        },
+      );
+    } else {
+      //return empty container
+      return Container();
+    }
+  }
+}
---
+++ lib/views/chat_page_view.dart
@@ -0,0 +1,126 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:rescuereach/components/chat_bubble.dart';
+import 'package:rescuereach/components/my_textfield.dart';
+
+import '../services/chat/chat_service.dart';
+
+class ChatPage extends StatefulWidget {
+  final String receiverUserEmail;
+  final String receiverUserID;
+  const ChatPage(
+      {super.key,
+      required this.receiverUserEmail,
+      required this.receiverUserID});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _messageController = TextEditingController();
+  final ChatService _chatService = ChatService();
+  final FirebaseAuth auth = FirebaseAuth.instance;
+  void sendMessage() async {
+    //only send messages if there is something to send
+    if (_messageController.text.isNotEmpty) {
+      await _chatService.sendMessage(
+          widget.receiverUserID, _messageController.text);
+      //clear the controller after sending the message
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.receiverUserEmail)),
+        body: Column(
+          children: [
+            //messages
+            Expanded(
+              child: _buildMessageList(),
+            ),
+            //user input
+            _buildMessageInput(),
+            const SizedBox(
+              height: 25,
+            ),
+          ],
+        ));
+  }
+
+  //build message list
+  Widget _buildMessageList() {
+    return StreamBuilder(
+      stream: _chatService.getMessages(
+          widget.receiverUserID, auth.currentUser!.uid),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error${snapshot.error}');
+        }
+        return ListView(
+          children: snapshot.data!.docs
+              .map((document) => _buildMessageItem(document))
+              .toList(),
+        );
+      },
+    );
+  }
+
+  //build  message item
+  Widget _buildMessageItem(DocumentSnapshot document) {
+    Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+    //align the  messages to the right if the sender is the current user, otherwise to the left
+    var alignment = (data['senderId'] == auth.currentUser!.uid)
+        ? Alignment.centerRight
+        : Alignment.centerLeft;
+    return Container(
+      alignment: alignment,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: (data['senderId'] == auth.currentUser!.uid)
+              ? CrossAxisAlignment.end
+              : CrossAxisAlignment.start,
+          mainAxisAlignment: (data['senderId'] == auth.currentUser!.uid)
+              ? MainAxisAlignment.end
+              : MainAxisAlignment.start,
+          children: [
+            Text(data['senderEmail']),
+            const SizedBox(
+              height: 5,
+            ),
+            ChatBubble(message: data['message']),
+          ],
+        ),
+      ),
+    );
+  }
+
+  //build message input
+  Widget _buildMessageInput() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: Row(
+        children: [
+          //text field
+          Expanded(
+              child: MyTextField(
+            controller: _messageController,
+            hintText: 'Enter message',
+            obscureText: false,
+          )),
+          //send button
+          IconButton(
+              onPressed: sendMessage,
+              icon: const Icon(
+                Icons.arrow_upward,
+                size: 40,
+              ))
+        ],
+      ),
+    );
+  }
+}
